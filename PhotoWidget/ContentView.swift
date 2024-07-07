0ee154670de441695da216614e4f1e910275beb9
Lishen2147 <lison2147@gmail.com>
2024-07-07 13:57:15 -0700

    Initial Commit

---
+++ PhotoWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoWidget
+//
+//  Created by Liyi Liu on 7/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoWidget/PhotoWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoWidgetApp.swift
+//  PhotoWidget
+//
+//  Created by Liyi Liu on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
